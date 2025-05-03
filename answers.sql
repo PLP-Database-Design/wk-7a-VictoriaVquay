5b9b977056d5b97ca56ea397dde70e9f9f9743c0
VictoriaVquay <victoria.mwende@yahoo.com>
2025-05-03 22:40:30 +0300

    Week 7 Assignment Resubmission

--- answers.sql
+++ answers.sql
@@ -1,11 +1,12 @@
 --Question 1 First Normal Form (1NF) SQL query
-CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(250),
-    Product VARCHAR(250)
+CREATE TABLE ProductDetail(
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
 );
 
-INSERT INTO Orders (OrderID, CustomerName) VALUES
+INSERT INTO ProductDetail(OrderID, CustomerName, Products) 
+VALUES
    (101, 'John Doe', 'Laptop'),
     (101, 'John Doe', 'Mouse'),
     (102, 'Jane Smith', 'Tablet'),
@@ -16,18 +17,32 @@ INSERT INTO Orders (OrderID, CustomerName) VALUES
 
 
 --Question 2 Second Normal Form (2NF)
-CREATE TABLE OrderDetails (
-    OrderID INT,
-    Product VARCHAR(250),
-    Quantity INT,
-    PRIMARY KEY (OrderID, Product),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
 );
 
-INSERT INTO OrderDetails (OrderID, Product, Quantity) VALUES
+INSERT INTO Orders (OrderID, CustomerName) 
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+CREATE TABLE Product(
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product), 
+    FOREIGN KEEY(OrderID)REFERENCES Orders(OrderID)
+);
+
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
 (101, 'Laptop', 2),
 (101, 'Mouse', 1),
 (102, 'Tablet', 3),
 (102, 'Keyboard', 1),
 (102, 'Mouse', 2),
 (103, 'Phone', 1);
+
+
